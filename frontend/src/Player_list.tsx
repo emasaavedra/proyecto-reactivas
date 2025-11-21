5b861d68186c87a755fec389ea47779e267758e0
emasaavedra <142761603+emasaavedra@users.noreply.github.com>
2025-11-21 13:47:38 -0300

    fix: Player_list.tsx implementa useEffect

--- frontend/src/Player_list.tsx
+++ frontend/src/Player_list.tsx
@@ -1,32 +1,29 @@
 import Player from "./components/player";
+import { useEffect } from "react";
 
 import "./Player_list.css";
 import usePlayerState from "./types/State";
 
 function Player_list() {
-  const { players, error } = usePlayerState(state => ({
-      players: state.players,
-      tournaments: state.tournaments,
-      error: state.error,
+  const { players, fetchPlayers, error } = usePlayerState(state => ({
+    players: state.players,
+    fetchPlayers: state.fetchPlayers,
+    error: state.error,
   }));
+
+  useEffect(() => {
+    if (players.length === 0) fetchPlayers();
+  }, [fetchPlayers, players.length]);
 
   return (
     <div className="player-list-container">
-       <h2>VCT Players</h2>
-      {players.length === 0 ? (
+      <h2>VCT Players</h2>
+
+      {error ? (
+        <p>Error: {error}</p>
+      ) : players.length === 0 ? (
         <p>No hay players pipipi</p>
       ) : (
-        <div className="players-grid">
-          {players.map((p, index) => (
-            <Player key={`${p.id}-${index}`} player={p} />
-          ))}
-        </div>
-      )}
-      { error ? (
-          <p>Error: {error}</p>
-        ) : players.length === 0 ? (
-          <p>No hay players pipipi</p>
-        ) : (
         <div className="players-grid">
           {players.map((p, index) => (
             <Player key={`${p.id}-${index}`} player={p} />
