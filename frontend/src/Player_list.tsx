8978e499ab3d2045da0f33e90807b2d473b22ec5
emasaavedra <142761603+emasaavedra@users.noreply.github.com>
2025-11-21 00:36:25 -0300

    reemplazado PlayerList para usar Zustand

--- frontend/src/Player_list.tsx
+++ frontend/src/Player_list.tsx
@@ -1,27 +1,32 @@
-import type { IPlayer } from "./types/Player";
-import playersService from "./services/playersService";
 import Player from "./components/player";
-import { useState, useEffect } from "react";
 
 import "./Player_list.css";
+import usePlayerState from "./types/State";
 
 function Player_list() {
-  const [players, setPlayers] = useState<IPlayer[]>([]);
-
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      const data = await playersService.getAll();
-      setPlayers(data);
-    };
-    fetchPlayers();
-  }, []);
+  const { players, error } = usePlayerState(state => ({
+      players: state.players,
+      tournaments: state.tournaments,
+      error: state.error,
+  }));
 
   return (
     <div className="player-list-container">
-      <h2>VCT Players</h2>
+       <h2>VCT Players</h2>
       {players.length === 0 ? (
         <p>No hay players pipipi</p>
       ) : (
+        <div className="players-grid">
+          {players.map((p, index) => (
+            <Player key={`${p.id}-${index}`} player={p} />
+          ))}
+        </div>
+      )}
+      { error ? (
+          <p>Error: {error}</p>
+        ) : players.length === 0 ? (
+          <p>No hay players pipipi</p>
+        ) : (
         <div className="players-grid">
           {players.map((p, index) => (
             <Player key={`${p.id}-${index}`} player={p} />
